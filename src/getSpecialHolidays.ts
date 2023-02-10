f0ad33fb203d06dfc534a51d2465ca7d0ea42671
koukibuu3 <koukibuu3@gmail.com>
2023-02-10 21:56:51 +0900

    feat: query文字列で受け取れるように

--- src/getSpecialHolidays.ts
+++ src/getSpecialHolidays.ts
@@ -1,8 +1,7 @@
 import { parse } from "https://deno.land/std@0.175.0/encoding/csv.ts"
 
-export const getSpecialHolidays = async (y: number|string, m: number|string) => {
-  const ym = `${y}/${m}`
+export const getSpecialHolidays = async (query: string) => {
   const syukujitsu = parse(await Deno.readTextFile(".data/syukujitsu.csv"))
 
-  return syukujitsu.filter(row => row[0].includes(ym)).map(row => row[0])
+  return syukujitsu.filter(row => row[0].includes(query)).map(row => row[0])
 }
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,33 @@
+import { format } from "https://deno.land/std@0.174.0/datetime/format.ts";
 import { serve } from "https://deno.land/std@0.175.0/http/server.ts"
+
 import { getSpecialHolidays } from "./getSpecialHolidays.ts"
 
 const handler = async (request: Request) => {
-  const searchParams = new URL(request.url).searchParams
+  let query = new URL(request.url).searchParams.get('q')
+  if (query && /^\d{6}$/.test(query) === false) {
+    return new Response('Invalid query', {
+      status: 400,
+      headers: {
+        "content-type": "text/json",
+      },
+    })
+  }
+  if (!query) {
+    query = format(new Date(), 'yyyyMM')
+  }
 
-  const year = searchParams.get("year") ?? new Date().getFullYear()
-  const month = searchParams.get("month") ?? new Date().getMonth() + 1
+  const [y, m] = [query.slice(0, 4), query.slice(4, 6)]
 
-  console.log(`year: ${year}, month: ${month}`)
+  console.log(`y: ${y}, m: ${m}`)
 
   const json = JSON.stringify({
-    year,
-    month,
+    year: y,
+    month: m,
     days: 31,
     workdays: 22,
     holidays: 8,
-    specialHolidays: (await getSpecialHolidays(year, month)).length,
+    specialHolidays: (await getSpecialHolidays(query)).length,
     workHours: 176,
   })
 
